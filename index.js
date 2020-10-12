f34c6c4b5f3db2ee8b2800c9eadb1054c4c789d2
techcoderx <engtianxi@outlook.com>
2020-10-12 18:25:40 +0800

    add block number info in /tx

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ MongoClient.connect(dbUrl, {useUnifiedTopology: true},(e,c) => {
     App.use(CORS())
 
     App.get('/tx/:txhash',(req,res) => {
-        db.collection('blocks').findOne({ "txs.hash": req.params.txhash }, { projection: { _id: 0, txs: { $elemMatch: { hash: req.params.txhash}}}},(error,tx) => {
+        db.collection('blocks').findOne({ "txs.hash": req.params.txhash }, { projection: { txs: { $elemMatch: { hash: req.params.txhash}}}},(error,tx) => {
             if (error)
                 res.status(500).send(error)
-            else if (tx && tx.txs)
-                res.send(tx.txs[0])
-            else
+            else if (tx && tx.txs) {
+                let result = tx.txs[0]
+                result.includedInBlock = tx._id
+                res.send(result)
+            } else
                 res.status(404).send({error: 'transaction not found'})
         })
     })
